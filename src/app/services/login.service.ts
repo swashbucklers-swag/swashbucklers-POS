c9c59530c28d5fe91e25ac26829bbe0ec418232c
dannyBernier <46949659+dannyBernier@users.noreply.github.com>
2021-04-21 15:00:35 -0500

    moved jwt and base url to environment.ts

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BASE_API_URL, JWT} from '../models/globalConstants';
+import { BASE_API_URL, JWT} from '../../environments/environment';
 import { JSONWebToken } from '../models/JSONWebToken';
 
 @Injectable({
