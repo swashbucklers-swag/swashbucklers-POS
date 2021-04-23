9796baa2cc7a4f6914d659dbfbbf326d2ffc7e50
dannyBernier <46949659+dannyBernier@users.noreply.github.com>
2021-04-23 17:06:40 -0500

    first pass at pagination

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -12,6 +12,10 @@ export class InventoryComponent implements OnInit {
   inventory:Inventory[];
   inventoryService:InventoryService;
   isLoading:boolean = true;
+  pageNumber:number = 0;
+  pageSize:number = 25;
+  totalPages:number;
+  totalElements:number;
 
   constructor(inventoryService:InventoryService) {
     this.inventoryService = inventoryService;
@@ -21,11 +25,39 @@ export class InventoryComponent implements OnInit {
     this.refreshInventory()
   }
 
+  pageFirst(){
+    this.pageNumber = 0;
+    this.refreshInventory();
+  }
+
+  pageLast(){
+    this.pageNumber = (this.totalPages - 1);
+    this.refreshInventory();
+  }
+
+  pageNext(){
+    if(this.pageNumber != (this.totalPages - 1)){
+      this.pageNumber++;
+      this.refreshInventory();
+    }
+  }
+
+  pagePrevious(){
+    if(this.pageNumber != 0){
+      this.pageNumber--;
+      this.refreshInventory();
+    }
+  }
+
   async refreshInventory(){
+    this.isLoading = true;
     try {
-      await this.inventoryService.getInventory().then(inv => {
+      await this.inventoryService.getInventory(this.pageSize, this.pageNumber).then(inv => {
         this.inventory = inv.content;
-        console.log(inv.content);
+        this.pageNumber = inv.pageable.pageNumber;
+        this.pageSize = inv.pageable.pageSize;
+        this.totalPages = inv.totalPages;
+        this.totalElements = inv.totalElements;
       });
     } catch (exception){
       alert("Failed to load inventory!\n\nIf this issue persists, contact your system administrator.")
